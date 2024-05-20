34fe4e11eae295e68d6c230704541337e727eb9e
mihir1910 <mihirraj715@gmail.com>
2024-05-20 12:11:53 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from cal_fun import do_add,do_div,do_mul,do_sub
+
+def main():
+  num1 = float(input("Enter the first number: "))
+  num2 = float(input("Enter the second number: "))
+
+  # Get the operation from the user.
+  operation = input("Enter the operation (+, -, *, /): ")
+
+  # Perform the operation and print the result.
+  if operation == "+":
+    result=do_add(num1,num2)
+  elif operation == "-":
+    result=do_sub(num1,num2)
+  elif operation == "*":
+    result=do_mul(num1,num2)
+  elif operation == "/":
+    result=do_div(num1,num2)
+  print("Result: ",result)
+
+
+if __name__ == "__main__":
+  main()
---
+++ cal_fun.py
@@ -0,0 +1,8 @@
+def do_add(a:float,b:float):
+    return a+b
+def do_sub(a:float,b:float):
+    return a-b
+def do_mul(a:float,b:float):
+    return a*b
+def do_div(a:float,b:float):
+    return a/b
